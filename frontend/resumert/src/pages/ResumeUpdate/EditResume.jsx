85a82e825a81e7406afaa0afdf9069531684a434
AvikRay1001 <avikray1001@gmail.com>
2025-06-02 13:58:42 +0530

    Day11

--- frontend/resumert/src/pages/ResumeUpdate/EditResume.jsx
+++ frontend/resumert/src/pages/ResumeUpdate/EditResume.jsx
@@ -10,11 +10,15 @@ import {
   LuDownload,
   LuPalette,
   LuSave,
+  LuTrash,
   LuTrash2,
 } from "react-icons/lu";
 import toast from "react-hot-toast";
 import DashboardLayout from '../../components/layouts/DashboardLayout';
 import TitleInput from '../../components/Inputs/TitleInput';
+import {useReactToPrint} from "react-to-print";
+import { useEffect } from 'react';
+import axiosInstance from '../../utils/axiosInstance';
 
 
 const EditResume = () => {
@@ -26,7 +30,7 @@ const EditResume = () => {
 
   const [baseWidth, setbaseWidth] = useState(800);
   const [openThemeSelector, setopenThemeSelector] = useState(false);
-  const [openPreviewModa, setopenPreviewModa] = useState(false);
+  const [openPreviewModal, setopenPreviewModal] = useState(false);
   const [currentPage, setcurrentPage] = useState("profile-info");
   const [progress, setprogress] = useState(0);
   const [resumeData, setresumeData] = useState({
@@ -82,7 +86,7 @@ const EditResume = () => {
           liveDemo: ""
       }
     ],
-    certification: [
+    certifications: [
         {
             title: "",
             issuer: "",
@@ -117,7 +121,34 @@ const EditResume = () => {
 
   const removeArrayItem = (section, index) => {};
 
-  const fetchResumeDetailsById = async() => {};
+  const fetchResumeDetailsById = async() => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.RESUME.GET_BY_ID(resumeId)
+      );
+
+      if(response.data && response.data.profileInfo) {
+        const resumeInfo = response.data;
+
+        setresumeData((prevState) => ({
+          ...prevState,
+          title: resumeInfo?.title || "Untitled",
+          template: resumeInfo?.template || prevState?.template,
+          profileInfo: resumeInfo?.profileInfo || prevState?.profileInfo,
+          contactInfo: resumeInfo?.contactInfo || prevState?.contactInfo,
+          workExperience: resumeInfo?.workExperience || prevState?.workExperience,
+          education: resumeInfo?.education || prevState?.education,
+          skills: resumeInfo?.skills || prevState?.skills,
+          projects: resumeInfo?.projects || prevState?.projects,
+          certifications: resumeInfo?.certifications || prevState?.certifications,
+          languages: resumeInfo?.languages || prevState?.languages,
+          interests: resumeInfo?.interests || prevState?.interests
+        }));
+      }
+    } catch (error) {
+      console.error("Error fetching resume details:", error);
+    }
+  };
 
   const uploadResumeImages = async() => {};
 
@@ -157,6 +188,85 @@ const EditResume = () => {
               }))
             }
           />
+
+          <div className='flex items-center gap-4'>
+            <button
+              className='btn-small-light'
+              onClick={() => setopenThemeSelector(true)}
+            >
+              <LuPalette className='text-[16px]'/>
+              <span className='hidden md:block'>Change Theme</span>
+            </button>
+
+            <button className='btn-small-light' onClick={handleDeleteResume}>
+              <LuTrash2 className='text-[16px]'/>
+              <span className='hidden md:block'>Delete</span>
+            </button>
+
+            <button
+              className='btn-small-light'
+              onClick={() => setopenPreviewModal(true)}
+            >
+              <LuDownload className='text-[16px]'/>
+              <span className='hidden md:block'>Preview & Download</span>
+            </button>
+          </div>
+        </div>
+
+
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+          <div className='bg-white rounded-lg border border-purple-100 overflow-hidden'>
+
+            {renderForm()}
+
+            <div className='mx-5'>
+              {errorMsg && (
+                <div className='flex items-center gap-2 text-[11px] font-medium text-amber-600 bg-amber-100 px-2 py-0.5 my-1 rounded'>
+                  <LuCircleAlert className='text-md'/> {errorMsg}
+                </div>
+              )}
+
+              <div className='flex items-end justify-end gap-3 mt-3 mb-5'>
+                <button
+                  className='btn-small-light'
+                  onClick={goBack}
+                  disabled={isLoading}
+                >
+                  <LuArrowLeft className='text-[16px]'/>
+                  Back
+                </button>
+                <button
+                  className='btn-small-light'
+                  onClick={uploadResumeImages}
+                  disabled={isLoading}
+                >
+                  <LuSave className='text-[16px]'/>
+                  {isLoading ? "Updating..." : "Save & Exit"}
+                </button>
+                <button
+                  className='btn-small'
+                  onClick={validateAndNext}
+                  disabled={isLoading}
+                >
+                  {currentPage === 'additionalInfo' && (
+                    <LuDownload className='text-[16px]'/>
+                  )}
+
+                  {currentPage === 'additionalInfo'
+                    ? "Preview & Download"
+                    : "Next Step"}
+                  
+                  {currentPage !== "additionalInfo" && (
+                    <LuArrowLeft className='text-[16px] rotate-180'/>
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div ref={resumeRef} className='h-[100vh]'>
+                  
+          </div>
         </div>
       </div>
     </DashboardLayout>
